16bb3f02bf4a2a91d792bd0f9c5e5357e7aec2c1
SardorIbodov <sardoribodov07@gmail.com>
2023-03-03 10:28:18 +0500

    3rd March challenges

--- 03-03-23/index.js
+++ 03-03-23/index.js
@@ -8,3 +8,59 @@ const uniTotal = (string) => {
     return v + c.charCodeAt();
   }, 0);
 };
+
+// Function 1 - hello world
+const greet = () => "hello world!";
+
+// Exclusive or (xor) operator
+const xor = (a, b) => {
+  if (typeof a === "boolean" && typeof b === "boolean") {
+    return a === b ? false : true;
+  }
+};
+const otherWayXor = (a, b) => !!(a ^ b);
+
+// Power
+const numberToPower = (number, power) => {
+  let result = number;
+  if (power === 0) return 1;
+  else if (power === 1) return number;
+  else {
+    for (let i = 2; i <= power; i++) {
+      result *= number;
+    }
+  }
+  return result;
+};
+const numberToPowerOtherWay = (number, power) => {
+  if (power === 1) {
+    return number;
+  }
+  return number * numberToPowerOtherWay(number, power - 1);
+};
+
+// Greet me
+const greetMe = (name) =>
+  `Hello ${name
+    .toLowerCase()
+    .replace(name.toLowerCase()[0], name.toLowerCase()[0].toUpperCase())}!`;
+
+// Chess
+function chessBoard(rows, columns) {
+  let result = [];
+  let line = [];
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= columns; j++) {
+      i % 2 === 0
+        ? j % 2 === 1
+          ? line.push("X")
+          : line.push("0")
+        : j % 2 === 1
+        ? line.push("0")
+        : line.push("X");
+    }
+    result.push(line);
+    line = [];
+  }
+  return result;
+}
