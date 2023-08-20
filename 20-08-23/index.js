8a7d9ee69a9da9674453866477b3d1b5a87c9057
Sardor <sardoribodov07@gmail.com>
2023-08-20 23:13:34 +0500

    20th August challenges

---
+++ 20-08-23/index.js
@@ -0,0 +1,20 @@
+// const getParticipants = (handshakes) => {
+// 	if(handshakes === 0) return 0;
+// 	else if(handshakes === 1) return 2;
+// 	else {
+// 		let result = 1;
+// 		while(result * (result - 1) / 2 < handshakes) {
+// 			result++;
+// 		}
+// 		return result;
+// 	}
+// }
+
+// const eliminateUnsetBits = (number) => {
+//   let newNumber = "";
+//   for (let i = 0; i < number.length; i++) {
+//     if (number[i] === "1") newNumber += number[i];
+//   }
+//   if (newNumber.length === 0) return 0;
+//   else return parseInt(newNumber, 2);
+// };
