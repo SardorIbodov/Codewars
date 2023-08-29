2ff94474c20a4ff7601f1707923ff2990e9497a5
Sardor <sardoribodov07@gmail.com>
2023-08-30 00:29:28 +0500

    29th challenges

---
+++ 29-08-23/index.js
@@ -0,0 +1,21 @@
+// function tailSwap(arr) {
+//   let first = arr[0].split(":")[0];
+//   let second = arr[0].split(":")[1];
+//   let third = arr[1].split(":")[0];
+//   let fourth = arr[1].split(":")[1];
+//   arr = [`${first}:${fourth}`, `${third}:${second}`];
+// 	return arr;
+// }
+// tailSwap(["abc:123", "cde:456"]);
+
+// var AmIAfraid = function(day, num){
+//   switch(day) {
+//     case "Monday": return num === 12;
+//     case "Tuesday": return num > 95;
+//     case "Wednesday": return num === 34;
+//     case "Thursday": return num === 0;
+//     case "Friday": return num % 2 === 0;
+//     case "Saturday": return num === 56;
+//     case "Sunday": return num === 666 || num === -666;
+//   }
+// }
