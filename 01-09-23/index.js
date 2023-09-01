14f36964d0313beff4f2e3a1ad9aa5c7b8efd4d9
Sardor <sardoribodov07@gmail.com>
2023-09-01 23:27:58 +0500

    1st September challenges

---
+++ 01-09-23/index.js
@@ -0,0 +1,12 @@
+// function areYouPlayingBanjo(name) {
+//   name = (name.startsWith("r") || name.startsWith("R")) ? `${name} plays banjo` : `${name} does not play banjo`;
+//   return name;
+// }
+
+// function chromosomeCheck(sperm) {
+//   return sperm.includes("Y") ? `Congratulations! You're going to have a son.` : `Congratulations! You're going to have a daughter.`;
+// }
+
+// function testEven(n) {
+// 	return n % 2 === 0;
+// }
