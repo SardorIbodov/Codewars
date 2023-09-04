d8588ae0d979090c10800589b44904aac5fa9737
Sardor <sardoribodov07@gmail.com>
2023-09-04 21:15:43 +0500

    4th September challenges

---
+++ 04-09-23/index.js
@@ -0,0 +1,12 @@
+// function twoArePositive(a, b, c) {
+//   if(a > 0 && b > 0 && c < 0) return true;
+//   else if(a > 0 && b < 0 && c > 0) return true;
+//   else if(a < 0 && b > 0 && c > 0) return true;
+//   else false;
+// }
+
+// function twoArePositive(a, b, c) {
+// 	return (a > 0) + (b > 0) + (c > 0) === 2;
+// }
+
+// const determineBase = f => Math.E ** (1 / f(Math.E));
